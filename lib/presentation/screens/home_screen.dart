5f4b2616f74f01f5d21eb5e2d2b0efc1e0d66a19
osamazakimohammed <121159354+osamazakimohammed@users.noreply.github.com>
2023-08-26 13:03:51 +0300

    Update home_screen.dart

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Home Screen"),
+        child: Text("OSAMA's Home Screen"),
       ),
     );
   }
